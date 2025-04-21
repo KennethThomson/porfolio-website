7000eb8703ce6e811751052ce69cf0c8744c03d8
KennethThomson <ken1212@bu.edu>
2025-04-21 00:42:25 -0400

    added background and styled container

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Pixelify_Sans } from "next/font/google";
+import "./globals.css";
+
+const pixelFont = Pixelify_Sans({
+  variable: "--font-pixel",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kenneth Thomson",
+  description: "Kenneth Thomson's Personal Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${pixelFont.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-screen bg-[url('/background.png')] bg-cover justify-center items-center">
+      <div className="container mb-5 flex items-center">
+        <h2>Welcome, traveller. What brings you here?</h2>
+      </div>
+      <div className="container"></div>
+      <div className="container">
+        <h1>Skills</h1>
+      </div>
+    </main>
+  );
+}
